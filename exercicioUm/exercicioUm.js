f5b917dcef198cdacc78d005beb5534b1f33e8b8
Maria Clara Giraldelli <mariaclaragiraldelli@hotmail.com>
2026-04-22 20:53:40 -0400

    Exercicio da aula 22/04

---
+++ exercicioUm/exercicioUm.js
@@ -0,0 +1,10 @@
+let nome = "Maria";
+let idade = 22;
+let sexo = true; // true para feminino, false para masculino
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Sexo: " + (sexo ? "Feminino" : "Masculino"));
+console.log("Tipo de dado de nome: " + typeof nome);
+console.log("Tipo de dado de idade: " + typeof idade);
+console.log("Tipo de dado de sexo: " + typeof sexo);
